0dbfc1e8944a37d9a7bc6f1b0d674ad00b0a6de3
Raj Singh <rajsingh22178@gmail.com>
2025-12-19 21:41:16 +0530

    sendgrid integeration

--- yearscore/settings.py
+++ yearscore/settings.py
@@ -147,3 +147,9 @@ LOGIN_REDIRECT_URL = "/dashboard/"
 LOGOUT_REDIRECT_URL = "/accounts/login/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "apikey"
+EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_API_KEY")
