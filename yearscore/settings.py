6c2cbd5e20a795c074a7418e6085d3072354d6a4
Raj Singh <rajsingh22178@gmail.com>
2025-12-27 21:58:33 +0530

    zzz

--- yearscore/settings.py
+++ yearscore/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # DEBUG = os.environ.get("DEBUG", "False") == "True"
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS",
     "localhost,127.0.0.1,build2026.onrender.com"
